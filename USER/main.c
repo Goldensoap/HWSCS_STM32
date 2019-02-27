92d2827a3e21e9512bcf0b9a66bb39d8d0674fb7
Goldensoap <33715060+Goldensoap@users.noreply.github.com>
2019-02-27 22:11:31 +0800

    改用9.00版本FreeRTOS，并完成移植和测试

--- USER/main.c
+++ USER/main.c
@@ -1,33 +1,104 @@
-#include "stm32f10x.h"
- void Delay(u32 count)
- {
-  u32 i=0;
-  for(;i<count;i++);
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "FreeRTOS.h"
+#include "task.h"
+/************************************************
+ ALIENTEK Mini STM32F103开发板 FreeRTOS实验2-1
+ FreeRTOS移植实验-库函数版本
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
 
- }
- int main(void)
- {	
-  GPIO_InitTypeDef  GPIO_InitStructure;
- 	
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOD, ENABLE);  //使能PA,PD端口时钟
+//任务优先级
+#define START_TASK_PRIO		1
+//任务堆栈大小	
+#define START_STK_SIZE 		128  
+//任务句柄
+TaskHandle_t StartTask_Handler;
+//任务函数
+void start_task(void *pvParameters);
+
+//任务优先级
+#define LED0_TASK_PRIO		2
+//任务堆栈大小	
+#define LED0_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED0Task_Handler;
+//任务函数
+void led0_task(void *pvParameters);
+
+//任务优先级
+#define LED1_TASK_PRIO		3
+//任务堆栈大小	
+#define LED1_STK_SIZE 		50  
+//任务句柄
+TaskHandle_t LED1Task_Handler;
+//任务函数
+void led1_task(void *pvParameters);
+
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4	 	 
+	delay_init();	    				//延时函数初始化	  
+	uart_init(115200);					//初始化串口
+	LED_Init();		  					//初始化LED
 	
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;				 //LED0-->PA.8 端口配置
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		 //IO口速度为50MHz
-  GPIO_Init(GPIOA, &GPIO_InitStructure);					 //根据设定参数初始化GPIOA.8
-  GPIO_SetBits(GPIOA,GPIO_Pin_8);						 //PA.8 输出高
+	 
+	//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler();          //开启任务调度
+}
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;	    		 //LED1-->PD.2 端口配置, 推挽输出
-  GPIO_Init(GPIOD, &GPIO_InitStructure);	  				 //推挽输出 ，IO口速度为50MHz
-  GPIO_SetBits(GPIOD,GPIO_Pin_2); 						  //PD.2 输出高 
-  while(1)
-	{
-	    GPIO_ResetBits(GPIOA,GPIO_Pin_8);
-	    GPIO_SetBits(GPIOD,GPIO_Pin_2);
-		Delay(3000000);
-		GPIO_SetBits(GPIOA,GPIO_Pin_8);
-		GPIO_ResetBits(GPIOD,GPIO_Pin_2);
-		Delay(3000000);
-	}
- }
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+    //创建LED0任务
+    xTaskCreate((TaskFunction_t )led0_task,     	
+                (const char*    )"led0_task",   	
+                (uint16_t       )LED0_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )LED0_TASK_PRIO,	
+                (TaskHandle_t*  )&LED0Task_Handler);   
+    //创建LED1任务
+    xTaskCreate((TaskFunction_t )led1_task,     
+                (const char*    )"led1_task",   
+                (uint16_t       )LED1_STK_SIZE, 
+                (void*          )NULL,
+                (UBaseType_t    )LED1_TASK_PRIO,
+                (TaskHandle_t*  )&LED1Task_Handler);         
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
+//LED0任务函数 
+void led0_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED0=~LED0;
+        vTaskDelay(300);
+    }
+}   
+
+//LED1任务函数
+void led1_task(void *pvParameters)
+{
+    while(1)
+    {
+        LED1=0;
+        vTaskDelay(200);
+        LED1=1;
+        vTaskDelay(800);
+    }
+}
 
