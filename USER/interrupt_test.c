534b101057b5da40f4d3b6aa53a3521dcf682acf
Goldensoap <33715060+Goldensoap@users.noreply.github.com>
2019-03-08 12:47:28 +0800

    添加开关中断测试代码interrupt_test，添加定时器文件，添加串口2配置,主程序测试双串口

---
+++ USER/interrupt_test.c
@@ -0,0 +1,72 @@
+#include "sys.h"
+#include "led.h"
+#include "delay.h"
+#include "timer.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+#define START_TASK_PRIO		1       //任务优先级
+#define START_STK_SIZE 		128     //任务堆栈大小	
+TaskHandle_t StartTask_Handler;     //任务句柄
+void start_task(void *pvParameters);//任务函数
+
+#define INTERRUPT_TASK_PRIO		2       //任务优先级
+#define INTERRUPT_STK_SIZE 		256     //任务堆栈大小	
+TaskHandle_t INTERRUPTTask_Handler;     //任务句柄
+void interrupt_task(void *pvParameters);//任务函数
+
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4	 	 
+	delay_init();	    				//延时函数初始化	  
+	uart_init(115200);					//初始化串口
+	LED_Init();		  					//初始化LED
+    TIM3_Int_Init(10000-1,7200-1);       //初始化定时器3，定时器周期1S
+    TIM5_Int_Init(10000-1,7200-1);       //初始化定时器3，定时器周期1S
+	
+	 
+	//创建开始任务
+    xTaskCreate((TaskFunction_t )start_task,            //任务函数
+                (const char*    )"start_task",          //任务名称
+                (uint16_t       )START_STK_SIZE,        //任务堆栈大小
+                (void*          )NULL,                  //传递给任务函数的参数
+                (UBaseType_t    )START_TASK_PRIO,       //任务优先级
+                (TaskHandle_t*  )&StartTask_Handler);   //任务句柄              
+    vTaskStartScheduler();          //开启任务调度
+}
+
+//开始任务任务函数
+void start_task(void *pvParameters)
+{
+    taskENTER_CRITICAL();           //进入临界区
+    //创建中断测试任务
+    xTaskCreate((TaskFunction_t )interrupt_task,     	
+                (const char*    )"interrupt_task",   	
+                (uint16_t       )INTERRUPT_STK_SIZE, 
+                (void*          )NULL,				
+                (UBaseType_t    )INTERRUPT_TASK_PRIO,	
+                (TaskHandle_t*  )&INTERRUPTTask_Handler);   
+       
+    vTaskDelete(StartTask_Handler); //删除开始任务
+    taskEXIT_CRITICAL();            //退出临界区
+}
+
+//中断测试任务函数
+void interrupt_task(void *pvParameters)
+{
+    static u32 total_num=0;
+    while(1){
+        total_num+=1;
+        if(total_num==5){
+            printf("disable interrupts......\r\n");
+            portDISABLE_INTERRUPTS();       //关闭中断
+            delay_xms(5000);
+            printf("enable interrupts......\r\n");
+            portENABLE_INTERRUPTS();        //打开中断
+					  total_num=0;
+        }
+        LED0=~LED0;
+        vTaskDelay(1000);
+    }
+}
--- USER/main.c
+++ USER/main.c
@@ -1,54 +1,35 @@
+#include "sys.h"
 #include "led.h"
 #include "delay.h"
-#include "sys.h"
+#include "timer.h"
 #include "FreeRTOS.h"
 #include "task.h"
-/************************************************
- ALIENTEK Mini STM32F103开发板 FreeRTOS实验2-1
- FreeRTOS移植实验-库函数版本
- 技术支持：www.openedv.com
- 淘宝店铺：http://eboard.taobao.com 
- 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
- 广州市星翼电子科技有限公司  
- 作者：正点原子 @ALIENTEK
-************************************************/
 
-//任务优先级
-#define START_TASK_PRIO		1
-//任务堆栈大小	
-#define START_STK_SIZE 		128  
-//任务句柄
-TaskHandle_t StartTask_Handler;
-//任务函数
-void start_task(void *pvParameters);
 
-//任务优先级
-#define LED0_TASK_PRIO		2
-//任务堆栈大小	
-#define LED0_STK_SIZE 		50  
-//任务句柄
-TaskHandle_t LED0Task_Handler;
-//任务函数
-void led0_task(void *pvParameters);
+#define START_TASK_PRIO		1       //任务优先级
+#define START_STK_SIZE 		128     //任务堆栈大小	
+TaskHandle_t StartTask_Handler;     //任务句柄
+void start_task(void *pvParameters);//任务函数
 
-//任务优先级
-#define LED1_TASK_PRIO		3
-//任务堆栈大小	
-#define LED1_STK_SIZE 		50  
-//任务句柄
-TaskHandle_t LED1Task_Handler;
-//任务函数
-void led1_task(void *pvParameters);
+#define UART1_TASK_PRIO		2       //任务优先级
+#define UART1_STK_SIZE 		256     //任务堆栈大小	
+TaskHandle_t UART1Task_Handler;     //任务句柄
+void uart1_task(void *pvParameters);//任务函数
+
+#define UART2_TASK_PRIO		3       //任务优先级
+#define UART2_STK_SIZE 		128     //任务堆栈大小	
+TaskHandle_t UART2Task_Handler;     //任务句柄
+void uart2_task(void *pvParameters);//任务函数
 
 int main(void)
 {
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4	 	 
 	delay_init();	    				//延时函数初始化	  
-	uart_init(115200);					//初始化串口
+	uart1_init(115200);					//初始化串口
+	uart2_init(115200);
 	LED_Init();		  					//初始化LED
-	
-	 
-	//创建开始任务
+
+	/*创建开始任务*/
     xTaskCreate((TaskFunction_t )start_task,            //任务函数
                 (const char*    )"start_task",          //任务名称
                 (uint16_t       )START_STK_SIZE,        //任务堆栈大小
@@ -58,47 +39,50 @@ int main(void)
     vTaskStartScheduler();          //开启任务调度
 }
 
-//开始任务任务函数
+/*开始任务任务函数*/
 void start_task(void *pvParameters)
 {
     taskENTER_CRITICAL();           //进入临界区
-    //创建LED0任务
-    xTaskCreate((TaskFunction_t )led0_task,     	
-                (const char*    )"led0_task",   	
-                (uint16_t       )LED0_STK_SIZE, 
+    /*创建中断测试任务*/
+    xTaskCreate((TaskFunction_t )uart1_task,     	
+                (const char*    )"uart1_task",   	
+                (uint16_t       )UART1_STK_SIZE, 
                 (void*          )NULL,				
-                (UBaseType_t    )LED0_TASK_PRIO,	
-                (TaskHandle_t*  )&LED0Task_Handler);   
-    //创建LED1任务
-    xTaskCreate((TaskFunction_t )led1_task,     
-                (const char*    )"led1_task",   
-                (uint16_t       )LED1_STK_SIZE, 
-                (void*          )NULL,
-                (UBaseType_t    )LED1_TASK_PRIO,
-                (TaskHandle_t*  )&LED1Task_Handler);         
+                (UBaseType_t    )UART1_TASK_PRIO,	
+                (TaskHandle_t*  )&UART1Task_Handler);   
+    /*创建uart2测试任务*/
+    xTaskCreate((TaskFunction_t )uart2_task,     	
+            (const char*    )"uart2_task",   	
+            (uint16_t       )UART2_STK_SIZE, 
+            (void*          )NULL,				
+            (UBaseType_t    )UART2_TASK_PRIO,	
+            (TaskHandle_t*  )&UART2Task_Handler);
     vTaskDelete(StartTask_Handler); //删除开始任务
     taskEXIT_CRITICAL();            //退出临界区
 }
 
-//LED0任务函数 
-void led0_task(void *pvParameters)
+/*中断测试任务函数*/
+void uart1_task(void *pvParameters)
 {
-    while(1)
-    {
-        LED0=~LED0;
-        vTaskDelay(300);
-    }
-}   
 
-//LED1任务函数
-void led1_task(void *pvParameters)
-{
-    while(1)
-    {
-        LED1=0;
-        vTaskDelay(200);
-        LED1=1;
-        vTaskDelay(800);
+    while(1){
+        printf("uart1 out put......\r\n");
+        LED0=~LED0;
+        vTaskDelay(1000);
     }
 }
 
+/*uart2测试任务函数*/
+void uart2_task(void *pvParameters)
+{
+    u8 TX[10]="0123456789";
+    while(1){
+        LED1=0;
+        for(int i=0; i<10; i++){
+             USART_SendData(USART2,TX[i]);
+             while(USART_GetFlagStatus(USART2, USART_FLAG_TC)==RESET);
+        }
+        LED1=1;
+        vTaskDelay(500);
+    }
+}
