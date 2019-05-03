303c2d2ec3c5a1da5554b5ed1c250619a177a8cf
Goldensoap <33715060+Goldensoap@users.noreply.github.com>
2019-05-04 03:34:56 +0800

    信息获取任务，修改上传信息格式

--- SYSTEM/msg_get_task.c
+++ SYSTEM/msg_get_task.c
@@ -11,9 +11,14 @@ TaskHandle_t MSG_Get_Task_Handler;
  * 本地变量
  */
 #define U32_MAX 0xffffffff
-#define SENSOR  1
-#define ROUTE   2
-#define LCD     3
+#define ALLTABLE    1
+#define SENSOR      2
+#define ROUTE       4
+#define LCD         3
+
+static void Send_Whole_Table( void );
+static void Send_Spec_Room_And_Type( u8 roomNum, u8 typeNum );
+static void LCD_Update( void );
 /*********************************************************************
  * 本地函数
  */
@@ -22,8 +27,6 @@ void MSG_Get_task(void *pvParameters)
 {
 	uint32_t NotifyValue=0;
 	BaseType_t err;
-    u8 Msg[MSG_UPLOAD_LEN];
-    for(int i =0;i<MSG_UPLOAD_LEN;i++)Msg[i]=0;
 
     while(1){
         /* code */
@@ -34,89 +37,119 @@ void MSG_Get_task(void *pvParameters)
         if(err==pdTRUE){
             switch ((NotifyValue&0x00ff0000)>>16)
             {
+                case ALLTABLE:
+                    Send_Whole_Table();
+                    break;
                 case SENSOR:
 				{
                     u8 roomNum = (NotifyValue&0x0000ff00)>>8;
                     u8 typeNum = NotifyValue&0x000000ff;
-                    SpaceNum_t *room_p = DataSheet;
-                    for( int i=0; i < ROOM_MAX; i++){
-                        if( room_p->space_num == roomNum){ //命中房间
-                            break;
-                        }else if(room_p->next == NULL){ //房间未找到
-#if _DEBUG
-                            printf("DEBUG:room not found\r\n");
-#endif
-                            break;
-                        }else room_p = room_p->next; //下一房间节点
-                    }
-                    SensorType_t *type_p = room_p->sensorType;
-                    for( int j=0; j<TYPE_MAX; j++){
-                        if( type_p->sensorType == typeNum ){ //命中类型
-                            SensorLabel_t *device = type_p->sensorLable;
-                            SensorData_t *data = NULL;
-                            while(device != NULL){ //输出类型下所有设备的最新数据
-                                data = device->sensorData;
-                                Msg[0]=( (device->sensorLabel)&0x00ff0000 )>>16;  //传感器编号
-                                Msg[1]=( (device->sensorLabel)&0x0000ff00 )>>8;   //内网地址 高位
-                                Msg[2]=( device->sensorLabel )&0x000000ff;        //内网地址 低位
-                                Msg[3]=( data->timestamp )>>24;                 //时间戳，大端
-                                Msg[4]=( (data->timestamp)&0x00ff0000 )>>16;    //时间戳
-                                Msg[5]=( (data->timestamp)&0x0000ff00 )>>8;     //时间戳
-                                Msg[6]=( data->timestamp )&0x000000ff;         //时间戳
-                                if ( (data->data)>255 ){
-                                    Msg[7]=( data->data )>>8;       // 传感数据 高位
-                                    Msg[8]=( data->data )&0x00FF;   // 传感数据 低位
-                                }else{
-                                    Msg[7]=(unsigned char)data->data;
-                                }
-                                err = xQueueSend(Msg_Upload_Queue,Msg,100); //发送至信息上传队列
-                                device = device->next;
-                            }
-                            break;
-                        }else if( type_p->next == NULL ){ //类型未找到
-#if _DEBUG
-                            printf("DEBUG:type not found\r\n");
-#endif
-                            break;
-                        }else type_p = type_p->next; //下一类型节点
-                    }
-                    if(err == pdPASS)for(int i =0;i<9;i++)Msg[i]=0;
+                    Send_Spec_Room_And_Type( roomNum, typeNum );
                     break;
 				}
                 case ROUTE:
                     break;
                 case LCD:
-                {
-                    uint16_t data_count=0;
-                    uint8_t room_count=0;
-                    uint8_t sensor_count=0;
-                    SpaceNum_t *room = DataSheet;
-                    SensorType_t *type = NULL;
-                    SensorLabel_t *device = NULL;
-                    SensorData_t *data = NULL;
-                    while(room != NULL){
-                        room_count ++;
-                        type = room->sensorType;
-                        while(type != NULL){
-                            device = type->sensorLable;
-                            while(device != NULL){
-                                data = device->sensorData;
-                                data_count += data->count;
-                                sensor_count ++;
-                                device = device->next;
-                            }
-                            type = type->next;
-                        }
-                        room = room->next;
-                    }
-                    LCD_display.totalroom = room_count;
-                    LCD_display.totaldata = data_count;
-                    LCD_display.totalsensor = sensor_count;
+                    LCD_Update();
 					break;
-                }
                 default:
                     break;
             }
         }
     }
+}
+
+static void Send_Whole_Table( void )
+{
+    // BaseType_t err;
+    // SpaceNum_t *room_p = DataSheet;
+    // char Msg[MSG_UPLOAD_LEN];
+    // for(int i =0;i<MSG_UPLOAD_LEN;i++)Msg[i]=0;
+}
+
+static void Send_Spec_Room_And_Type( u8 roomNum, u8 typeNum )
+{
+	BaseType_t err;
+    char Msg[MSG_UPLOAD_LEN];
+    for(int i =0;i<MSG_UPLOAD_LEN;i++)Msg[i]=0;
+
+    SpaceNum_t *room_p = DataSheet;
+    for( int i=0; i < ROOM_MAX; i++){
+        if( room_p->space_num == roomNum){ //命中房间
+            break;
+        }else if(room_p->next == NULL){ //房间未找到
+
+            #if _DEBUG
+            printf("{\"DEBUG\":\"room not found\"}\r\n");
+            #endif
+
+            break;
+        }else room_p = room_p->next; //下一房间节点
+    }
+    SensorType_t *type_p = room_p->sensorType;
+    for( int j=0; j<TYPE_MAX; j++){
+        if( type_p->sensorType == typeNum ){ //命中类型
+            SensorLabel_t *device = type_p->sensorLable;
+            SensorData_t *data = NULL;
+            while(device != NULL){ //输出类型下所有设备的最新数据
+                data = device->sensorData;
+                /*
+                  * 
+                  * 设备内网地址
+                  * 传感器编号
+                  * 传感类型
+                  * 数据内容
+                  * 时间戳
+                  * 
+                */
+                sprintf(Msg,"{\"SENSOR\":{\"Space\":%u,\"Device\":\"%x\",\"Sensor\":%u,\"Type\":%u,\"Data\":%u,\"Time\":%u}}\r\n",\
+                    roomNum,\
+                    (u16)((device->sensorLabel)&0x0000ffff),\
+                    (u8)(((device->sensorLabel)&0x00ff0000)>>16),\
+                    typeNum,\
+                    (u16)(data->data),\
+                    (u32)(data->timestamp));
+                err = xQueueSend(Msg_Upload_Queue,Msg,100); //发送至信息上传队列
+                device = device->next;
+            }
+            break;
+        }else if( type_p->next == NULL ){ //类型未找到
+
+            #if _DEBUG
+            printf("{\"DEBUG\":\"type not found\"}\r\n");
+            #endif
+
+            break;
+        }else type_p = type_p->next; //下一类型节点
+    }
+    if(err == pdPASS)for(int i =0;i<9;i++)Msg[i]=0;
+}
+
+static void LCD_Update( void )
+{
+    uint16_t data_count=0;
+    uint8_t room_count=0;
+    uint8_t sensor_count=0;
+    SpaceNum_t *room = DataSheet;
+    SensorType_t *type = NULL;
+    SensorLabel_t *device = NULL;
+    SensorData_t *data = NULL;
+    while(room != NULL){
+        room_count ++;
+        type = room->sensorType;
+        while(type != NULL){
+            device = type->sensorLable;
+            while(device != NULL){
+                data = device->sensorData;
+                data_count += data->count;
+                sensor_count ++;
+                device = device->next;
+            }
+            type = type->next;
+        }
+        room = room->next;
+    }
+    LCD_display.totalroom = room_count;
+    LCD_display.totaldata = data_count;
+    LCD_display.totalsensor = sensor_count;
 }
